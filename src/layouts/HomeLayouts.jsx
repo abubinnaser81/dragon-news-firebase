fec435ef054a0129e1558c84ec4f521f36a32d91
Abu Bin Naser <abubinnaser81@gmail.com>
2026-06-03 14:52:28 +0600

    sticky

--- src/layouts/HomeLayouts.jsx
+++ src/layouts/HomeLayouts.jsx
@@ -19,7 +19,7 @@ const HomeLayout = () => {
       </header>
 
       <main className="w-11/12 mx-auto my-3 grid grid-cols-12 gap-5">
-        <aside className="col-span-3">
+        <aside className="col-span-3 sticky top-2 h-fit">
           <LeftAside />
         </aside>
 
@@ -27,7 +27,7 @@ const HomeLayout = () => {
           <Outlet />
         </section>
 
-        <aside className="col-span-3">
+        <aside className="col-span-3 sticky top-2 h-fit">
           <RighAside />
         </aside>
       </main>
